cd7e6fa9c28bff2297723401e69b83fc2222dcc6
mbayajames <jmuthiora244@gmail.com>
2025-03-09 00:33:39 -0800

    Fix issue in index.js to improve stability

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-// const express = require("express");
+const express = require("express");
 
-// const app = express();
-// const PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-// app.get("/", (req, res) => {
-//     res.send("Hello, Node.js Server!");
-// });
+app.get("/", (req, res) => {
+    res.send("Hello, Node.js Server!");
+});
 
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    console.log("Server is running!");
+
+});
